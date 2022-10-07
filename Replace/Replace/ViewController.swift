cd211aa8b7127f9cafeb3dc70bece2ec74ff5d5b
Bohdan Buhai <>
2022-10-07 05:50:02 +0300

    Add fille

---
+++ Replace/Replace/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Replace
+//
+//  Created by Bohdan Buhai on 05.10.2022.
+//
+//var string = "foo!"
+//string.replace("!", with: "?")
+//print(string)
+
+//var stringToReplace = "This my string"
+//if let range = stringToReplace.range(of: "my") {
+//   stringToReplace?.replaceSubrange(range, with: "your")
+//}
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var someWordTF: UITextField!
+    @IBOutlet weak var wordChengTo: UITextField!
+    
+    @IBOutlet weak var chengedWordOrTexs: UILabel!
+    
+    @IBOutlet weak var textViewText: UITextView! //{
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUi()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func replaceButton(_ sender: UIButton) {
+        if validity() {
+            if textViewText.text.contains(someWordTF.text ?? "") {
+                chengedWordOrTexs.text = textViewText.text.replacingOccurrences(of: someWordTF.text ?? "", with: wordChengTo.text ?? "")
+            }
+            
+        }
+       
+    }
+    
+    private func showAlert(massage: String) {
+        let alert = UIAlertController(title: "Alert", message: massage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func validity() -> Bool {
+        if someWordTF.text?.isEmpty == true {
+            showAlert(massage: "Find text can not be empty")
+            return false
+        }
+        if wordChengTo.text?.isEmpty == true {
+            showAlert(massage: "Replace text can not be empty")
+            return false
+        }
+        if textViewText.text?.isEmpty == true {
+            showAlert(massage: "Text can not be empty")
+            return false
+        }
+        return true
+    }
+    
+    func setupUi() {
+        textViewText.layer.borderWidth = 2
+        textViewText.layer.borderColor = UIColor.gray.cgColor
+        textViewText.layer.cornerRadius = 8
+    }
+    
+}
+
